865feb514f5e13269618c3b24da2bbeb8fdc38ec
Vladk <vladkontsevykh@gmail.com>
2021-03-08 18:14:27 +0300

    build.gradle fix: Now can be launched correctly.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    application
 }
 
 group = "me.vlade"
@@ -11,8 +12,14 @@ repositories {
     mavenCentral()
 }
 
-
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "13"
+}
+
+application{
+    mainClass.set("MainKt")
 }
